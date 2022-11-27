6640b2fc9627a7ce37ae0986ed8aa6e26a85432c
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-11-27 08:11:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -68,6 +68,23 @@ async def report_user(bot, message):
         if success:
             await message.reply_text("Your Requested Movie Name Sent To Group admins!")
 
+@Client.on_message(filters.command('Glink') & filters.private &  filters.chat(Config.BOT_OWNER))
+async def gen_invite(bot, message):
+    if len(message.command) == 1:
+        return await message.reply('Give me a chat id')
+    chat = message.command[1]
+    try:
+        chat = int(chat)
+    except:
+        return await message.reply('Give Me A Valid Chat ID')
+    try:
+        link = await bot.create_chat_invite_link(chat)
+    except ChatAdminRequired:
+        return await message.reply("Invite Link Generation Failed, Iam Not Having Sufficient Rights")
+    except Exception as e:
+        return await message.reply(f'Error {e}')
+    await message.reply(f'Here is your Invite Link {link.invite_link}')
+
 @Client.on_message(filters.command("users") & filters.private &  filters.chat(Config.BOT_OWNER))
 async def total_users(_, event: Message):
     total_users = await db.total_users_count()
