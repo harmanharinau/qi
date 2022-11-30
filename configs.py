ae8ffdce904f3eac47473b401f3c6966989c7029
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-11-30 17:25:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,35 +7,35 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.getenv("API_ID", "13335517" ))
-    API_HASH = os.getenv("API_HASH", "76b6a02d5f039628ac07e72108638c1a")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5400296177:AAGh8wMOAFenNQDfeckQjsl8eBILVZFGNlw")
+    API_ID = int(os.getenv("API_ID", "12124605" ))
+    API_HASH = os.getenv("API_HASH", "5cf3577d85fd02286535ec2296934287")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "5563802574:AAH3Sli-SL_Nu4hefr-ko7A0TW-7RTWs2a8")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "MdiskSearchRobot")
-    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001656400798))
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "potter_moviebot")
-    BOT_OWNER = int(os.getenv("BOT_OWNER", "5272889145"))
-#    OWNER_USERNAME = os.getenv("OWNER_USERNAME", POTTER_00)
-    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "potterhub")
+    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOGkBuzKcbvS6Q0eq8NZm5O2dwH4fFWy86831W5WefCF2bBISyR7TGnCHYiSAI7UzOzlZ1jeCJ2dLhZjo9oFTe1PNYnrpt88HuHWxOUwxGHF87hW6QVTfP-V3iuKg0OJSAQjs3L1B0EJ-a9yptc5fv5sbIwOOzewOkHFsrMmKB8vCoGW9XAS-qfcDBWGDzSh4RQUo7lNtAAhe_r2cXSKCPx4eWSSGyTDCosjc-VQndi6elYxPy6bljQCK7_xitvTqOVCx4u17E7__2lVOYt650lnm4fYn6ZikAbTDGOwoeeeI_adTXx_Fkw9-YwF-b4ZT9AdxXIJmk8pNqtskTZEtxX5aZdg=")
+    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001697543381)
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "yourmdiskbot")
+    BOT_OWNER = int(os.getenv("BOT_OWNER", "1883570185"))
+#    OWNER_USERNAME = os.getenv("OWNER_USERNAME", cyniteofficial)
+    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "cynitebackup")
 #    GROUP_USERNAME = os.getenv("GROUP_USERNAME")
     START_MSG = os.getenv("START_MSG", "<b>Hᴇʏ ! 😃  I'ᴍ A Bᴏᴛ Fᴏʀ Sᴇɴᴅɪɴɢ Fʀᴏᴍ Yᴏᴜʀ Cʜᴀɴɴᴇʟ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ.😚  Yᴏᴜ Cᴀɴ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ.☺️  Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅</b>")
-    START_PHOTO = os.getenv("START_PHOTO", "https://ibb.co/WD43gS5")
+    START_PHOTO = os.getenv("START_PHOTO", "https://telegra.ph/file/7d357b72c29a6aa21fb78.jpg")
     HOME_TEXT = os.getenv("HOME_TEXT")
-    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-1001556642705")
-    DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://telegraph:mdisk@cluster0.lm7dyxe.mongodb.net/?retryWrites=true&w=majority")
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001819001558"))
-    RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 5))
+    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-1001249072794")
+    DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://yourmdisk:yourmdisk@cluster0.eqvuwsx.mongodb.net/?retryWrites=true&w=majority")
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001805890096"))
+    RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 20))
     BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "True")
-    UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "potterhub")
+    UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "cynitemovies")
     FORCE_SUB = os.getenv("FORCE_SUB", "False")
     AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 300))
-    MDISK_API = os.getenv("MDISK_API", "12334")
+    MDISK_API = os.getenv("MDISK_API", "Qu7jX9V0Sn3q1JHdxjPp")
     REPLIT_USERNAME = os.environ.get("REPLIT_USERNAME", None) # your replit username 
     REPLIT_APP_NAME = os.environ.get("REPLIT_APP_NAME", None) # your replit app name 
     REPLIT = f"https://{REPLIT_APP_NAME.lower()}.{REPLIT_USERNAME}.repl.co" if REPLIT_APP_NAME and REPLIT_USERNAME else False
     PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "300"))
     VERIFIED_TIME  = int(os.getenv("VERIFIED_TIME", "31"))
-    ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT", "  <b>I ᴏɴʟʏ ꜱʜᴀʀᴇ ᴛʜᴇ ᴘᴏꜱᴛ ꜰʀᴏᴍ ᴘᴇᴏᴘʟᴇ'ꜱ ᴄʜᴀɴɴᴇʟ! ᴡʜᴏ ᴍᴀᴅᴇ ᴍᴇ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ, i ᴅᴏ ɴᴏᴛ ꜱᴛᴏʀᴇ ᴀɴʏ ꜰɪʟᴇꜱ ᴏʀ ᴛᴇxᴛ ɪɴ  ᴍʏ ᴅᴀᴛᴀʙᴀꜱᴇ.✅ \n ᴅᴍ ꜰᴏʀ ᴀɴʏ Qᴜᴇʀʏ @potter_00</b> ")
+    ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT", "  <b>I ᴏɴʟʏ ꜱʜᴀʀᴇ ᴛʜᴇ ᴘᴏꜱᴛ ꜰʀᴏᴍ ᴘᴇᴏᴘʟᴇ'ꜱ ᴄʜᴀɴɴᴇʟ! ᴡʜᴏ ᴍᴀᴅᴇ ᴍᴇ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ, i ᴅᴏ ɴᴏᴛ ꜱᴛᴏʀᴇ ᴀɴʏ ꜰɪʟᴇꜱ ᴏʀ ᴛᴇxᴛ ɪɴ  ᴍʏ ᴅᴀᴛᴀʙᴀꜱᴇ.✅ \n ᴅᴍ ꜰᴏʀ ᴀɴʏ Qᴜᴇʀʏ @cyniteofficial</b> ")
     ABOUT_HELP_TEXT = """
 🍓 RᴇQᴜɪʀᴇᴍᴇɴᴛ ᴛᴏ ᴜꜱᴇ ᴛʜɪꜱ ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ!
 
@@ -44,10 +44,10 @@ class Config(object):
 🍓 Sᴛᴇᴘ 2 - ʙᴏᴛ ᴋᴏ ᴀᴘɴᴇ ɢʀᴏᴜᴘ ᴀᴜʀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴋᴀ ᴀᴅᴍɪɴ ʙᴀɴᴀɴᴀ ʜᴏɢᴀ.
 
 🍓 Sᴛᴇᴘ 3 - ɢʀᴏᴜᴘ ᴍᴇ "/verify" ᴛʏᴘᴇ ᴋᴀʀ ᴋᴇ ꜱᴇɴᴅ ᴋᴀʀɴᴀ ʜᴏɢᴀ!
-ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ. @potter_00
+ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ. @cyniteofficial
 
 🍓 Sᴛᴇᴘ 4 - ɢʀᴏᴜᴘ ᴍᴇ "/database - ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ɪᴅ" ᴛʏᴘᴇ ᴋᴀʀ ᴋᴇ ꜱᴇɴᴅ ᴋᴀʀɴᴀ ʜᴏɢᴀ.
-ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ʙʜɪ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ @potter_00
+ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ʙʜɪ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ @cyniteofficial
 
 🍓 Sᴛᴇᴘ 5 - ᴀʙ ᴀᴘᴋᴏ ᴀᴘɴᴇ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴍᴇ ᴘᴏꜱᴛ ᴅᴀʟɴɪ ʜᴏɢɪ,
 ᴊɪꜱꜱᴇ ᴋɪ ᴀɢᴀʀ ɢʀᴏᴜᴘ ᴍᴇ ᴋᴏɪ ᴜꜱᴇʀ ꜱᴇᴀʀᴄʜ ᴋᴀʀᴇ ᴛᴏ ʏᴇ ʙᴏᴛ ᴜɴ ᴜꜱᴇʀ ᴋᴇ Qᴜᴀʀʏ ᴋᴏ ꜱᴀᴍᴀᴊʜ ᴋᴇ ᴀᴘᴋᴇ ᴄʜᴀɴɴᴇʟ ꜱᴇ ᴘᴏꜱᴛ ᴜᴛʜᴀ ᴋᴇ ᴜɴʜᴇ ᴅᴇ ᴘᴀʏᴇ.
@@ -55,5 +55,5 @@ class Config(object):
 
 🍓 Nᴏᴛᴇ : Bᴏᴛ ᴀᴅᴍɪɴ ᴀᴘᴋᴇ ᴄʜᴀɴɴᴇʟ ᴍᴇ ᴊᴏɪɴ ʜᴏɴᴇ ᴄʜᴀʜɪʏᴇ,
 ᴀɢᴀʀ ʙᴏᴛ ᴀᴅᴍɪɴ ᴀᴘᴋᴀ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ɴʜɪ ᴋᴀʀ ʀᴀʜᴇ ʜᴀɪɴ ᴛᴏ ᴜɴʜᴇ ᴘᴇʀꜱᴏɴᴀʟ ᴍꜱɢ ᴋᴀʀᴇɴ.
-👉 @Potter_00
+👉 @cyniteofficial
 """
