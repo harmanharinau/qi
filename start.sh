4b224e6b4c9108ac3daf44381eef31b22b6092d0
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-12-01 07:27:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/harmanharinau/yourmdisk.git /yourmdisk
-cd /yourmdisk
+git clone https://github.com/harmanharinau/pirosearcher.git /pirosearcher
+cd /pirosearcher
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
