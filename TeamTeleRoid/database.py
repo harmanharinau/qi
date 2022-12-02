6d3abd2cd66c175b6a864dabdc29e579052c950b
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-12-02 07:06:48 +0530

    Update database.py

--- TeamTeleRoid/database.py
+++ TeamTeleRoid/database.py
@@ -43,13 +43,6 @@ class Database:
         all_users = self.col.find({})
         return all_users
 
-    async def total_chat_count(self):
-        count = await self.grp.count_documents({})
-        return count
-    
-    async def get_all_chats(self):
-        return self.grp.find({})
-
     async def delete_user(self, user_id):
         self.col.delete_many({'id': int(user_id)})
 
