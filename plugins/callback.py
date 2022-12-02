d26790900d70eace61a0555044bb8fb689bbb53a
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-12-02 17:35:47 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -67,7 +67,7 @@ async def request_access_handler(c:Client,query: CallbackQuery):
         return await query.message.reply("You already have access to this Bot")
     else: 
      try:
-                invite_link =  await c.create_chat_invite_link(int(db_channel))
+                invite_link =  await {link.invite_link}
             except Exception as e:
         REPLY_MARKUP = InlineKeyboardMarkup([
             [
