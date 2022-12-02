c717d8a58f83ef1f3c2def71a762a7269cfe2066
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-12-02 18:02:44 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -69,7 +69,9 @@ async def request_access_handler(c:Client,query: CallbackQuery):
         try:
                 invite_link =  await bot.create_chat_invite_link
 
-        try: 
+        except Exception as e:
+            print(e)
+
         REPLY_MARKUP = InlineKeyboardMarkup([
             [
                 InlineKeyboardButton('Allow', callback_data=f'give_access#{query.message.chat.id}#{query.from_user.id}'),
