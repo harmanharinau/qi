6df12e85ba3fd6767ede8bb3db38d68b7f0d2a15
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-12-02 18:20:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -124,7 +124,6 @@ async def gen_invite(bot, message):
         return await message.reply('Give Me A Valid Chat ID')
     try:
         link = await bot.create_chat_invite_link(chat)
-    except ChatAdminRequired:
         return await message.reply("Invite Link Generation Failed, Iam Not Having Sufficient Rights")
     except Exception as e:
         return await message.reply(f'Error {e}')
