a0e0cf9f890b35f2037a4dac76bf8b79d8f0cba4
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-12-02 17:50:13 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -66,10 +66,7 @@ async def request_access_handler(c:Client,query: CallbackQuery):
     if user["has_access"] and await db.is_group_verified(group_id):
         return await query.message.reply("You already have access to this Bot")
     else: 
-     try:
-                invite_link =  await bot.create_chat_invite_link
-            except Exception as e:
-        return await m.reply_text("Your Request Has Been Sent")
+     
         REPLY_MARKUP = InlineKeyboardMarkup([
             [
                 InlineKeyboardButton('Allow', callback_data=f'give_access#{query.message.chat.id}#{query.from_user.id}'),
